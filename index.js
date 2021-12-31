d9812f150885707f212c5b20a2842e886da553e8
Sham6910 <luvuyosilubane50@gmail.com>
2021-12-31 21:38:59 +0200

    Better RPC lol

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
-const RPC = require('discord-rpc')
-const rpc = new RPC.Client({
+const { Client } = require('discord-rpc')
+const rpc = new Client({
     transport: 'ipc'
 })
 
 rpc.on("ready", () => {
     rpc.setActivity({
-        details: "An idiot who codes.",
-        state: "I make RimBot and Bot for only myself.",
-        startTimestamp: new Date(),
-        largeImageKey: "sham",
-        largeImageText: "Sham",
-        smallImageKey: "rb",
-        smallImageText: "RimBot",
-        buttons : [{label : "Invite" , url : "https://rimbot.xyz/invite/"},{label : "Website" , url : "https://rimbot.xyz/"}]
+        details: "eZaR Simulator 2022",
+        state: "The new Discord Multi-Bot.",
+        largeImageKey: "ezar_blurple",
+        largeImageText: "eZaR",
+        smallImageKey: "ezar_green",
+        smallImageText: "amaze bot",
+        buttons: [{ label: "Website", url: "https://ezar.shamdev.xyz/" }, { label: "Support Server", url: "https://discord.gg/2Usu8E5KK4" }]
     })
 
-    console.log("successfully activated the RPC")
+    console.log("RPC is on.")
 })
 
 rpc.login({
-    clientId: '886912056366891068'
+    clientId: '906816722676371496'
 })
