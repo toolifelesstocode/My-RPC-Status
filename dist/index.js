9997c86214d87e32527bb357ee2d93c56e97dfae
Sham <thesham@protonmail.com>
2022-05-09 21:40:31 +0200

    fix: Git-ignored the `dist` folder

--- dist/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const dotenv_1 = require("dotenv");
-const Client_1 = require("./utils/Client");
-(0, dotenv_1.config)();
-const client = new Client_1.RPCClient();
-client.login({ clientId: process.env.CLIENTID });
